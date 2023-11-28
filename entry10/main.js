1bd8fccc8b33c07c2e88927f3fac5553a727c9a1
parke889 <parke889@newschool.edu>
2023-11-28 06:00:24 -0500

    1

--- entry10/main.js
+++ entry10/main.js
@@ -18,3 +18,38 @@ function changeImage() {
 
     box.src = randomImage;
 }
+
+
+
+// when i click i want an image replace the box
+// when i click i want it to be a randomly generated image
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drop(event) {
+    event.preventDefault();
+    var box = document.getElementById('box');
+
+    // Get the dropped file
+    var file = event.dataTransfer.files[0];
+
+    // Check if a file is dropped
+    if (file) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            // Set the dropped image as the source
+            box.src = e.target.result;
+            images.push(e.target.result);
+        };
+
+        // Read the dropped file as a data URL
+        reader.readAsDataURL(file);
+    }
+}
+
+function drag(event) {
+    event.dataTransfer.setData('text', event.target.src);
+}
